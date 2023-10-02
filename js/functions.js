1a8e4c652f6f6df9a73d23e50ec11a071ae3aa99
Эмиль <yagufarove@gmail.com>
2023-10-02 11:35:56 +0500

    Сделал рефакторинг относительно стандартов e

--- js/functions.js
+++ js/functions.js
@@ -1,27 +1,22 @@
- const getLineLength = (line , maxLength) => line.length <= maxLength;
- function isPalindrome(line) {
-   line = line.toLowerCase().replaceAll(' ', '');
+const getLineLength = (line, maxLength) => line.length <= maxLength;
 
-   for (let i = 0; i < line.length; i++) {
-     if (line[line.length - 1 - i] !== line[i]) {
-       return false;
-     }
-   }
-
-   return true;
- }
-
- function getNumber(data) {
-
-   data = data.toString().replaceAll(' ','');
-   let result ='';
-
-   for (let i = 0; i < data.length; i++) {
-     if (!isNaN(data[i])) {
-       result += data[i];
-     }
-   }
-
-   return parseInt(result);
- }
+const isPalindrome = (line)  => {
+  line = line.toLowerCase().replaceAll(' ', '');
+  for (let i = 0; i < line.length; i++) {
+    if (line[line.length - 1 - i] !== line[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+function getNumber(data) {
+  data = data.toString().replaceAll(' ','');
+  let result ='';
+  for (let i = 0; i < data.length; i++) {
+    if (!isNaN(data[i])) {
+      result += data[i];
+    }
+  }
+  return parseInt(result, 10);
+}
 
