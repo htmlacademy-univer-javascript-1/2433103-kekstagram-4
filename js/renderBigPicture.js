79fadcc38121f6cc21d1308db3e9453941ed00a9
Emil Yagufarov <yagufarove@gmail.com>
2023-11-22 16:15:38 +0500

    добавил отображение комментариев

--- js/renderBigPicture.js
+++ js/renderBigPicture.js
@@ -9,6 +9,7 @@ const socialCaption = bigPicture.querySelector('.social__caption');
 const socialCommentCount = bigPicture.querySelector('.social__comment-count');
 const commentsLoader = bigPicture.querySelector('.comments-loader');
 const cancelButton = bigPicture.querySelector('.big-picture__cancel');
+const socialComments = bigPicture.querySelector('.social__comments');
 const closeModal = (evt) => {
   if (isEscapeKey(evt) || evt.type === 'click') {
     bigPicture.classList.add('hidden');
@@ -17,12 +18,26 @@ const closeModal = (evt) => {
     cancelButton.removeEventListener('click', closeModal);
   }
 };
+
+const drawComment = (comment) => {
+  const commentHtml = `<li class="social__comment">
+                               <img class="social__picture"
+                                src="${comment.avatar}" alt="${comment.name}" width="35" height="35">
+                               <p class="social__text">${comment.message}</p>
+                               </li>`;
+  socialComments.insertAdjacentHTML('beforeend',commentHtml );
+};
+const drawComments = (comments) => {
+  socialComments.innerHTML = '';
+  comments.forEach((comment) => drawComment(comment));
+};
 const openModal = (image) => {
   bigPicture.classList.remove('hidden');
   bigPictureImg.querySelector('img').src = image.url;
   likesCount.textContent = image.likes;
   commentsCount.textContent = image.description.length.toString();
   socialCaption.textContent = image.description;
+  drawComments(image.comments);
   socialCommentCount.classList.add('hidden');
   commentsLoader.classList.add('hidden');
   document.body.classList.add('modal-open');
