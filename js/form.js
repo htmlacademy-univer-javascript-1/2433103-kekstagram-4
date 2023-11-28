3cb64acc17ff6193a2814611026b3dfa2f9be02b
Emil Yagufarov <yagufarove@gmail.com>
2023-11-28 18:21:45 +0500

    Добавил модуль validators.js для валидации комметария и хештегов. Импортировал функции из validators.js и использовал в form.js

--- js/form.js
+++ js/form.js
@@ -1,8 +1,6 @@
 import {isEscapeKey} from './util.js';
-
-const MAX_HASHTAGS_COUNT = 5;
-const MAX_DESCRIPTIONS_COUNT = 120;
-const SEPARATOR = ' ';
+import {isHashtagsCountCorrect, isDescriptionCountCorrect, isSeparatorCorrect, isHashtagCorrect,
+  isHashtagNotRepeat, MAX_DESCRIPTIONS_COUNT, MAX_HASHTAGS_COUNT} from './validators.js';
 
 const imgUploadForm = document.body.querySelector('.img-upload__form');
 const uploadFile = imgUploadForm.querySelector('#upload-file');
@@ -10,49 +8,17 @@ const imgUploadOverlay = imgUploadForm.querySelector('.img-upload__overlay');
 const uploadCancel = imgUploadForm.querySelector('.img-upload__cancel');
 const textHashtags = imgUploadForm.querySelector('.text__hashtags');
 const textDescription = imgUploadForm.querySelector('.text__description');
-const regExpHashtag = new RegExp('^#[0-9A-Za-zа-яА-Я]{1,19}$');
-const regExpDescription = new RegExp(`^.{0,${MAX_DESCRIPTIONS_COUNT}}$`);
 const pristine = new Pristine(imgUploadForm, {
   classTo: 'img-upload__field-wrapper',
   errorTextParent: 'img-upload__field-wrapper',
   errorTextClass: 'img-upload__error-text'
 });
 
-pristine.addValidator(textHashtags, (value) => {
-  if (value !== '') {
-    const hashtags = value.toLowerCase().trim().split(SEPARATOR);
-    return hashtags.every((hashtag) => regExpHashtag.test(hashtag));
-  }
-  return true;
-}, 'Один или несколько введеных хештегов - некорректен');
-
-pristine.addValidator(textHashtags, (value) => {
-  if (value !== '') {
-    const hashtags = value.toLowerCase().trim().split(SEPARATOR);
-    const hashtagsCount = value.split('').filter((sym) => sym === '#').length;
-    return hashtagsCount === hashtags.length;
-  }
-  return true;
-}, 'Введен неверный разделитель');
-
-pristine.addValidator(textHashtags, (value) => {
-  if (value !== '') {
-    const hashtagsCount = value.toLowerCase().trim().split(SEPARATOR).length;
-    return hashtagsCount <= MAX_HASHTAGS_COUNT;
-  }
-  return true;
-}, `Максимальное количество хештегов - ${MAX_HASHTAGS_COUNT}`);
-
-pristine.addValidator(textHashtags, (value) => {
-  if (value !== '') {
-    const hashtags = value.toLowerCase().trim().split(SEPARATOR);
-    const hashtagsSet = new Set(hashtags);
-    return hashtags.length === hashtagsSet.size;
-  }
-  return true;
-}, 'Хештеги регистронезависимы и не должны повторяться');
-
-pristine.addValidator(textDescription, (value) => regExpDescription.test(value),
+pristine.addValidator(textHashtags, isHashtagCorrect, 'Один или несколько введеных хештегов - некорректен');
+pristine.addValidator(textHashtags, isSeparatorCorrect, 'Введен неверный разделитель');
+pristine.addValidator(textHashtags, isHashtagsCountCorrect, `Максимальное количество хештегов - ${MAX_HASHTAGS_COUNT}`);
+pristine.addValidator(textHashtags, isHashtagNotRepeat, 'Хештеги регистронезависимы и не должны повторяться');
+pristine.addValidator(textDescription, isDescriptionCountCorrect,
   `Максимальная длина комментария - ${MAX_DESCRIPTIONS_COUNT} символов`);
 const closeModal = (evt) => {
   if ((isEscapeKey(evt) && document.activeElement !== textHashtags
@@ -77,3 +43,4 @@ imgUploadForm.addEventListener('submit', (evt) => {
     evt.preventDefault();
   }
 });
+
